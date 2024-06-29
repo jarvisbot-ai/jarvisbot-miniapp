3ef89d9b20459b5b0c1e519c3c55998a22008671
Guo Changzhu <guochangzhu@qq.com>
2024-06-29 15:28:49 +0800

    update

--- setting.js
+++ setting.js
@@ -1,10 +1,3 @@
-/*
- * @Author: weihua.wang
- * @Date: 2024-01-24 20:36:07
- * @LastEditors: weihua.wang
- * @LastEditTime: 2024-06-19 15:59:04
- * @Description: Do not edit
- */
 const API_BASE_URL = '',
     SSE_B = 'https://mini.jarvisbot.ai/oca/v1/n4/chat/sendSse/sse',
     USE_LOCAL = false,
